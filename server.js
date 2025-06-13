9f7544f9343df63a795053275ed683344ed8a75d
Fiqhi Naura <135235139+fiqhinaura@users.noreply.github.com>
2025-06-13 15:55:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); 
 app.use(router)
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
